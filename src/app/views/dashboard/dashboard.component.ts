0fe83867f63c1b5e7f742a5a9f1562f7844357fa
Kerem Isik <kerem.ishik@gmail.com>
2025-09-25 00:00:07 +0300

    refactor: remove unused imports from components
    Clean up component files by removing unused imports to improve code maintainability and reduce bundle size

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  CardBodyComponent,
-  CardComponent,
-  CardHeaderComponent,
-  ColComponent,
-  RowComponent
-} from '@coreui/angular';
 
 @Component({
   templateUrl: 'dashboard.component.html',
-  styleUrls: ['dashboard.component.scss'],
-  imports: [
-    CardComponent, 
-    CardBodyComponent, 
-    CardHeaderComponent,
-    RowComponent, 
-    ColComponent
-  ]
+  styleUrls: ['dashboard.component.scss']
 })
 export class DashboardComponent {
   // Boş dashboard - içerik eklemek için buraya kod yazabilirsiniz
