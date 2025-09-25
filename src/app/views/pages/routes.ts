3d264e3d2c87d1f41f3e34f6ae7bee01cddf6906
Kerem Isik <kerem.ishik@gmail.com>
2025-09-25 09:40:32 +0300

    refactor(routes): remove unused 500 and register page routes
    The 500 error page and registration page routes were removed as they are no longer needed in the application. This simplifies the routing configuration.

--- src/app/views/pages/routes.ts
+++ src/app/views/pages/routes.ts
@@ -9,24 +9,10 @@ export const routes: Routes = [
     }
   },
   {
-    path: '500',
-    loadComponent: () => import('./page500/page500.component').then(m => m.Page500Component),
-    data: {
-      title: 'Page 500'
-    }
-  },
-  {
     path: 'login',
     loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
     data: {
       title: 'Login Page'
-    }
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
-    data: {
-      title: 'Register Page'
     }
   }
 ];
